4d16672df7e8754382e42132beb99c6a73a859cc
ryan <rkulyassa@gmail.com>
2024-04-04 12:23:44 -0400

    updated demo

--- quadtree-js/Quadtree.js
+++ quadtree-js/Quadtree.js
@@ -1,37 +1,83 @@
 class Point {
-    constructor(x, y) {
+    constructor(x, y, radius) {
         this.x = x;
         this.y = y;
+        this.radius = radius;
+        // this.dx = Math.random() * cellVelocity*2 - cellVelocity;
+        // this.dy = Math.sign(this.dx) * (1 - this.dx**2);
+        this.dx = 0;
+        this.dy = 0;
     }
 
-    draw(ctx) {
+    getBoundingRect() {
+        return new Rect(this.x, this.y, this.radius*2, this.radius*2);
+    }
+
+    update() {
+        this.dx = canvas.width/2 - this.x;
+        this.dy = canvas.height/2 - this.y;
+        this.x += this.dx * acceleration;
+        this.y += this.dy * acceleration;
+        // this.dy += 9.8;
+        // this.x += this.dx;
+        // this.y += this.dy;
+
+        if (this.x - this.radius < 0) {
+            this.x = this.radius;
+            this.dx = -this.dx;
+        }
+        if (this.x + this.radius > canvas.width) {
+            this.x = canvas.width - this.radius;
+            this.dx = -this.dx;
+        }
+        if (this.y - this.radius < 0) {
+            this.y = this.radius;
+            this.dy = -this.dy;
+        }
+        if (this.y + this.radius > canvas.height) {
+            this.y = canvas.height - this.radius;
+            this.dy = -this.dy;// * 0.65;
+        }
+    }
+
+    draw() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 5, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fillStyle = '#FF0000';
         ctx.fill();
     }
 }
 
 class Rect {
-    constructor(x, y, width, height) {
+    constructor(x, y, w, h) {
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
+        this.w = w;
+        this.h = h;
     }
 
     contains(point) {
-        return point.x > this.x - this.width &&
-               point.x < this.x + this.width &&
-               point.y > this.y - this.height &&
-               point.y < this.y + this.height;
+        return point.x >= this.x - this.w &&
+               point.x <= this.x + this.w &&
+               point.y >= this.y - this.h &&
+               point.y <= this.y + this.h;
+    }
+
+    intersects(other) {
+        return this.x - this.w <= other.x + other.w &&
+               this.x + this.w >= other.x - other.w &&
+               this.y - this.h <= other.y + other.h &&
+               this.y + this.h >= other.y - other.h;
+
     }
 }
 
 class Quadtree {
-    constructor(boundary, capacity) {
+    constructor(boundary, capacity, maxDepth, parent) {
         this.boundary = boundary;
         this.capacity = capacity;
+        this.maxDepth = maxDepth;
+        this.depth = parent ? parent.depth + 1 : 1;
         this.points = [];
         this.divided = false;
         this.branches = [null, null, null, null]; // NW, NE, SW, SE
@@ -40,27 +86,32 @@ class Quadtree {
     insert(point) {
         if (!this.boundary.contains(point)) return;
 
-        if (this.points.length < this.capacity && !this.divided) {
+        if (this.depth > this.maxDepth) {
             this.points.push(point);
         } else {
-            if (!this.divided) {
-                this.subdivide();
-            }
-            for (const branch of this.branches) {
-                branch.insert(point);
+            if (this.points.length < this.capacity && !this.divided) {
+                this.points.push(point);
+            } else {
+                if (!this.divided) {
+                    this.subdivide();
+                }
+                for (const branch of this.branches) {
+                    branch.insert(point);
+                }
             }
         }
     }
 
     subdivide() {
+        if (this.depth > this.maxDepth) return;
         const x = this.boundary.x;
         const y = this.boundary.y;
-        const w = this.boundary.width;
-        const h = this.boundary.height;
-        this.branches[0] = new Quadtree(new Rect(x - w/2, y - h/2, w/2, h/2), this.capacity);
-        this.branches[1] = new Quadtree(new Rect(x + w/2, y - h/2, w/2, h/2), this.capacity);
-        this.branches[2] = new Quadtree(new Rect(x - w/2, y + h/2, w/2, h/2), this.capacity);
-        this.branches[3] = new Quadtree(new Rect(x + w/2, y + h/2, w/2, h/2), this.capacity);
+        const w = this.boundary.w;
+        const h = this.boundary.h;
+        this.branches[0] = new Quadtree(new Rect(x - w/2, y - h/2, w/2, h/2), this.capacity, this.maxDepth, this);
+        this.branches[1] = new Quadtree(new Rect(x + w/2, y - h/2, w/2, h/2), this.capacity, this.maxDepth, this);
+        this.branches[2] = new Quadtree(new Rect(x - w/2, y + h/2, w/2, h/2), this.capacity, this.maxDepth, this);
+        this.branches[3] = new Quadtree(new Rect(x + w/2, y + h/2, w/2, h/2), this.capacity, this.maxDepth, this);
 
         for (const point of this.points) {
             for (const branch of this.branches) {
@@ -73,21 +124,42 @@ class Quadtree {
         this.points = [];
         this.divided = true;
     }
-    
-    draw(ctx) {
-        const x = this.boundary.x;
-        const y = this.boundary.y;
-        const w = this.boundary.width;
-        const h = this.boundary.height;
-        ctx.strokeRect(x - w, y - h, w*2, h*2);
+
+    query(range, found) {
+        if (!found) found = [];
+
+        if (!this.boundary.intersects(range)) return found;
 
         for (const point of this.points) {
-            point.draw(ctx);
+            if (range.contains(point)) {
+                found.push(point);
+            }
         }
 
         if (this.divided) {
             for (const branch of this.branches) {
-                branch.draw(ctx);
+                branch.query(range, found);
+            }
+        }
+
+        return found
+    }
+    
+    draw() {
+        const x = this.boundary.x;
+        const y = this.boundary.y;
+        const w = this.boundary.w;
+        const h = this.boundary.h;
+        ctx.lineWidth = 1;
+        ctx.strokeRect(x - w, y - h, w*2, h*2);
+
+        for (const point of this.points) {
+            point.draw();
+        }
+
+        if (this.divided) {
+            for (const branch of this.branches) {
+                branch.draw();
             }
         }
     }
--- quadtree-js/main.js
+++ quadtree-js/main.js
@@ -1,35 +1,63 @@
 const canvas = document.getElementsByTagName('canvas')[0];
 const ctx = canvas.getContext('2d', { alpha: false });
-const dpr = window.devicePixelRatio;
-const rect = canvas.getBoundingClientRect();
-canvas.width = rect.width * dpr;
-canvas.height = rect.height * dpr;
+// const dpr = window.devicePixelRatio;
+// const rect = canvas.getBoundingClientRect();
+// canvas.width = rect.width * dpr;
+// canvas.height = rect.height * dpr;
 
-const CAPACITY = 4;
-const POINTS = 1000;
+const cellVelocity = 2;
+const capacity = 4;
+const acceleration = 0.1;
 
-const quadtree = new Quadtree(new Rect(canvas.width/2, canvas.width/2, canvas.width/2, canvas.width/2), CAPACITY);
-
-for (let i = 0; i < POINTS; i++) {
-    const point = new Point(Math.random()*canvas.width, Math.random()*canvas.height);
-    quadtree.insert(point);
-}
-quadtree.draw(ctx);
-
-function addPoint(x, y) {
-    const point = new Point(x, y);
-    quadtree.insert(point);
-    quadtree.draw(ctx);
-}
+let points = [];
 
 function addPoints(n) {
     for (let i = 0; i < n; i++) {
-        const point = new Point(Math.random()*canvas.width, Math.random()*canvas.height);
-        quadtree.insert(point);
-        quadtree.draw(ctx);
+        const point = new Point(Math.random() * canvas.width, Math.random() * canvas.height, Math.floor(Math.random()*10+10));
+        points.push(point);
     }
 }
 
-ctx.scale(dpr, dpr);
-canvas.style.width = `${rect.width}px`;
-canvas.style.height = `${rect.height}px`;
+function resolveCollision(a, b) {
+    const dx = b.x - a.x;
+    const dy = b.y - a.y;
+    const d = Math.sqrt(dx*dx + dy*dy);
+    if (d > a.radius + b.radius) return;
+    const lerp = (a.radius + b.radius - d)/2;
+    a.x -= dx/d * lerp;
+    a.y -= dy/d * lerp;
+    b.x += dx/d * lerp;
+    b.y += dy/d * lerp;
+}
+
+let quadtree;
+
+function step() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    quadtree = new Quadtree(new Rect(canvas.width/2, canvas.width/2, canvas.width/2, canvas.width/2), capacity, 5, null);
+    for (const point of points) {
+        quadtree.insert(point);
+    }
+    for (const a of points) {
+        const pointsResult = quadtree.query(a.getBoundingRect(), null);
+        // console.log(pointsResult);
+        // console.log(a.getBoundingRect());
+        // console.log(pointsResult);
+        for (const b of pointsResult) {
+            if (a === b) continue;
+            for (let i = 0; i < 4; i++) {
+                resolveCollision(a, b);
+            }
+            // point.update(this.rootBoundary);
+        }
+        a.update();
+    }
+    quadtree.draw(ctx);
+    requestAnimationFrame(step);
+}
+
+requestAnimationFrame(step);
+
+// ctx.scale(dpr, dpr);
+// canvas.style.width = `${rect.width}px`;
+// canvas.style.height = `${rect.height}px`;
